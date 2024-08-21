d3ee6ce1282e0a8d45ae65a5aaa0ad405c851740
wade-cheng <wadecheng3301@gmail.com>
2024-08-20 21:53:17 -0700

    start frontend; index.html
    - working place to revert to if needed
    - going to start adding things to the db now

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="article_index"),
+    path("authors", views.index, name="article_author_index"),
+    path("tags", views.index, name="article_category_index"),
+    path("issues", views.index, name="article_issues_index"),
+    path("editor/issue", views.index, name="article_issues_edit"),
+    path("post/<str:slug>/<int:pk>", views.index, name="article_detail"),
+    path("category/<category>/", views.index, name="article_category"),
+    path("author/<author>/", views.index, name="article_author"),
+    path("issue/<int:vol>/<int:num>", views.index, name="article_issue"),
+    path('login/', views.index, name='login'),
+    path('logout/', views.index, name='logout'),
+    path('signup/', views.index, name='signup'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.http import HttpResponseRedirect
+from articles.forms import (
+    CommentForm,
+)
+from articles.models import (
+    Article,
+    Comment,
+    Author,
+    Issue,
+    Category,
+)
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    articles = Article.objects.all().order_by("-created_on").filter(published=True)
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles/index.html", context)
