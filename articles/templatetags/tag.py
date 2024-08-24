ff7752979aeb4209ecbc299e02183f971d4d66bf
wade-cheng <wadecheng3301@gmail.com>
2024-08-23 23:15:07 -0400

    fix images. just need to re-add them all now

--- articles/templatetags/tag.py
+++ articles/templatetags/tag.py
@@ -1,5 +1,6 @@
 import markdown
 from django import template
+from django.conf import settings
 import re
 register = template.Library()
 
@@ -12,10 +13,12 @@ def add_img_folder(folder):
     def add_img(filename):
         #return f"<img src=\"/static/{folder}/{filename}\" width=500>"
         # TODO EXPLICIT STATIC USED ie not static django tag
-        return f"<img src=\"/static/{folder}/{filename}\" style=\"width: 50%; margin-left: 25%; height: auto;\"><br>\n\n"
+        return f"<img src=\"{settings.MEDIA_URL}{folder}{filename}\" style=\"width: 50%; margin-left: 25%; height: auto;\"><br>\n\n"
     return add_img
 
 def replace_double_braces(arg, folder):
+    # print(f"got folder: {folder}") # "first" arg
+    # print(f"got arg: {arg}") # the content that was piped in
     pattern = r'\{\{([^}]*)\}\}'
     replace = add_img_folder(folder)
     modified_text = re.sub(pattern, lambda x: replace(x.group(1)), arg)
