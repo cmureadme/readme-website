643feca20ee99e617162d7e554cd746a19e29a6f
wade-cheng <wadecheng3301@gmail.com>
2025-01-27 14:39:01 -0500

    configging whitenoise

--- readme_website/settings.py
+++ readme_website/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = "staticfiles/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+WHITENOISE_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
