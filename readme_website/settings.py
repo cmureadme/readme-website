3d5aba54707e9173675ee251fd6f1596a9e5550f
ZeRana <xxzeranaxx@gmail.com>
2025-01-30 11:39:33 -0500

    i did this from the command line idk if the commit is fucked, anyway fixed some ui stuff

--- readme_website/settings.py
+++ readme_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dorr)@&&^dz79b_do&7t83vgl903y8+_2qce7gipxr2@wsv=iw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     ".localhost",
