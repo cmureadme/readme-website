5f761c5dd6717430d08b42c44a9e918354cb91b5
Ryan Tosh <ryan.tosh@rto.run>
2025-11-11 21:36:02 -0500

    Update allowed hosts

--- readme_website/settings/host.py
+++ readme_website/settings/host.py
@@ -6,6 +6,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# TODO: change this to the exact (sub)domains we should expect in Host for prod/staging
 # https://docs.djangoproject.com/en/5.2/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.dev.cmureadme.com'
+]
