0e7ad25a2a770287c96e69b24c14ab13dac03f49
ZeRana <xxzeranaxx@gmail.com>
2025-11-22 15:30:46 -0500

    Error handling for paginator

--- magazine/views.py
+++ magazine/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.conf import settings
 from django.db.models import Q
 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from random import shuffle
 
