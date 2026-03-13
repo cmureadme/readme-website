022a203b346caf58d8984393a1d590cf0ef35ff4
ZeRana <xxzeranaxx@gmail.com>
2026-03-13 18:00:14 -0400

    Added the cost (free and ) to the issue form)

--- magazine/models.py
+++ magazine/models.py
@@ -147,6 +147,8 @@ class Issue(models.Model):
         help_text="Defaults to the current day, change this if the issue was not published the same day you are uploading",
     )
     paid_for = models.ForeignKey("PaidFor", related_name="paid_for", null=True, on_delete=models.PROTECT)
+    free = models.CharField(max_length=CHARFIELD_MAX_LENGTH, null=True, help_text="This is what goes before : Free")
+    three_dollars = models.CharField(max_length=CHARFIELD_MAX_LENGTH, null=True, help_text="This is what goes before : $3")
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
